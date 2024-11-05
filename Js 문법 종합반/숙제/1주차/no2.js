34cdb7b02d37cb3a8933d5c619094a242b05709d
strobus <116132298+BlueStrobus@users.noreply.github.com>
2024-11-05 20:18:45 +0900

    1.2문제

---
+++ Js 문법 종합반/숙제/1주차/no2.js
@@ -0,0 +1,17 @@
+/*
+1. 음수, 양수 true, fals로 구분하기
+2. 정수배열의 값을 +_하기
+3. 결과 반횐
+*/
+
+function solution(absolutes, signs) {
+    var answer = 0;
+    for (let i = 0; i < signs.length; i++) {
+        if (signs[i]) {  // 양수(true)면 참
+            answer += absolutes[i];
+        } else {
+            answer -= absolutes[i];
+        }
+    }
+    return answer;
+}
