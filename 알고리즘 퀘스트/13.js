34e2996375d1514da5059ba8dff1c92aa294294f
strobus <116132298+BlueStrobus@users.noreply.github.com>
2024-11-15 08:22:56 +0900

    14번 내용이어서 13번으로 수정

--- 알고리즘 퀘스트/13.js
+++ 알고리즘 퀘스트/13.js
@@ -1,31 +1,4 @@
-function solution(n) {
-  if (n === 0) return 0;
-  let sum = 0;
-  for (let i = 1; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) {
-      sum += i + (i !== n / i ? n / i : 0);
-    }
-  }
-  return sum;
+function solution(n){
+    let strN = n.toString(10);
+    return [...strN].reduce((sum, num) => sum + Number(num), 0);
 }
-
-/*
-function solution(n) {
-    return n === 0 ? 0 : Array.from({ length: n }, (_, i) => i + 1)
-                         .filter(v => n % v === 0)
-                         .reduce((a, b) => a + b, 0);
-}
-*/
-
-/*
-function solution(n) {
-    if (n <= 0) return 0;
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            sum += i; // 약수를 바로 더함
-        }
-    }
-    return sum;
-}
- */
